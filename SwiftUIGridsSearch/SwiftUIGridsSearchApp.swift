c648f4c51922562d622267de9394ecfe0b5d4fa9
Ashish Tyagi <ashisht@dewsolutions.in>
2020-10-14 09:08:38 +0530

    Initial Commit

---
+++ SwiftUIGridsSearch/SwiftUIGridsSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGridsSearchApp.swift
+//  SwiftUIGridsSearch
+//
+//  Created by Ashish Tyagi on 14/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGridsSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
